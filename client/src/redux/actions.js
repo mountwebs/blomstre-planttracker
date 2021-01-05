8c19ce861954c3b34e9045985440a9108226f1b0
mountwebs <mountwebs@gmail.com>
2021-01-05 16:17:16 +0100

    added fetch and fail reducers

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,4 +1,7 @@
 import * as actionTypes from './actionTypes';
+import axios from 'axios';
+
+const apiBaseUrl = 'http://localhost:8000/api/plants/';
 
 export const waterPlant = (plantId, date) => {
   return {
@@ -12,5 +15,31 @@ export const addPlant = (plant) => {
   return {
     type: actionTypes.ADD_PLANT,
     plant,
+  }
+}
+
+export const setPlants = (plants) => {
+  return {
+    type: actionTypes.SET_PLANTS,
+    plants
+  }
+}
+
+export const fetchPlantsFailed = () => {
+  return {
+    type: actionTypes.FETCH_PLANTS_FAILED
+  }
+}
+
+export const initPlants = () => {
+  return dispatch => {
+    axios.get(apiBaseUrl)
+      .then(response => {
+        dispatch(setPlants(response.data))
+      })
+      .catch(error => {
+        dispatch(fetchPlantsFailed())
+      })
+
   }
 }
