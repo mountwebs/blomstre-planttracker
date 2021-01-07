30df2c3a00473cae854b02bf0ecafa0729e9a493
mountwebs <mountwebs@gmail.com>
2021-01-07 12:44:52 +0100

    added morgan

--- server/app.js
+++ server/app.js
@@ -1,27 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const plantRouter = require('./plants/routes');
-const cors = require('cors');
-// var bodyParser = require('body-parser');
-// const bodyParser = require('body-parser');
-// const morgan = require('morgan');
+const plantRouter = require("./plants/routes");
+const cors = require("cors");
+const morgan = require("morgan");
+
+app.use(morgan("tiny"));
 
 app.use(express.json());
 app.use(cors());
 
-app.use('/api/plants', plantRouter);
+app.use("/api/plants", plantRouter);
 
 app.use((req, res, next) => {
   next(createError(404));
 });
 
-// error handler
 app.use((err, req, res, next) => {
-  // render the error page
   res.status(err.status || 500);
-  res.end('error');
+  res.end("error");
 });
 
 app.listen(8000, () => {
-  console.log('Listening on port 8000');
+  console.log("Listening on port 8000");
 });
